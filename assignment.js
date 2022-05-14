920e5c5f5a35a5f62c462f3873bc70d56566e347
oscar.ahmed11@gmail.com <oscar.ahmed11@gmail.com>
2022-05-14 18:44:29 +0300

    first commet in javaScript tutorial

---
+++ assignment.js
@@ -0,0 +1,31 @@
+const country = "Iraq";
+const continent = "Asia";
+let population = 40000000;
+population /= 2;
+population +=1;
+
+let finlandPopulation = 6000000;
+const isIsland = false;
+const language = "Arabic";
+
+let discription = `Portugal is in Europe, and its 11 million people speak portuguese`;
+
+
+console.log(typeof country);
+// console.log(typeof continent);
+console.log(population);
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(population);
+// console.log(population > finlandPopulation);
+// console.log(population < 33000000);
+
+if (population > 33000000){
+    console.log(`Portugal's population is above average`)
+}
+else{
+    console.log(`Portugal's population is 22 million below average`)
+}
+
+
+
---
+++ challenge1.js
@@ -0,0 +1,35 @@
+let markMass = 78;
+let markHeight = 1.69;
+
+let johnMass = 92;
+let johnHeight = 1.95;
+
+let markBmi = markMass / (markHeight**2);
+
+let johnBmi = johnMass / (johnHeight**2);
+
+
+console.log(markBmi, johnBmi, markBmi > johnBmi);
+
+if (markBmi > johnBmi){
+    console.log(`Mark's BMI ${markBmi} is higher than John's ${johnBmi}!`)
+}
+else{
+    console.log(`John's BMI ${johnBmi} is higher than Mark's ${markBmi}!`)
+}
+
+
+
+// markMass   = 95 ;
+// markHeight = 1.88;
+// johnMass   = 85;
+// johnHeight = 1.76;
+
+// markBmi= markMass / (markHeight**2);
+// johnBmi= johnMass / (johnHeight**2);
+
+// console.log (markBmi, johnBmi, markBmi> johnBmi);
+
+
+
+
