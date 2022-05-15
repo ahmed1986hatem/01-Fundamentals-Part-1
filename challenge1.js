d2b5bbb0baf694df8cb9e5c2310bd31e1e6bfc18
oscar.ahmed11@gmail.com <oscar.ahmed11@gmail.com>
2022-05-15 23:05:05 +0300

    tip challenge is done

--- challenge1.js
+++ challenge1.js
@@ -67,6 +67,18 @@ else{
     console.log("Both are Equals 🤝");
 }
 
+let tip;
+let bill=275;
+
+bill >=50 && bill<= 300? tip = bill * 0.15: tip = bill * 0.2;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value
+${tip+bill}`)
+
+
+
+
+
 
 
 
